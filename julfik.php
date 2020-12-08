ed70e0d9febde58da1a1a0f129573f261eabcd14
TheJulfikarpoc <75205271+TheJulfikarpoc@users.noreply.github.com>
2020-12-08 23:09:09 +0600

    Create julfik.php

---
+++ julfik.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!-- saved from url=(0096)http://iklanku.web.id/z.php?fbclid=IwAR35j4wxO3r_moOS21Qrxy9RQYxR_Ff6GMoKBmzb9ezdR3wzv_lITydJxNw -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>pwned by thejulfikar</title>
+<meta property="og:image" content="https://cdn.idntimes.com/content-images/community/2019/07/img-20190704-004932-183-530a0421764a7532a7fb6d7e6ac2fe81_600x400.JPG">
+<link rel="icon" type="image/png" href="https://www.google.com/search?biw=1366&amp;bih=662&amp;tbm=isch&amp;sa=1&amp;ei=pVYGXYqMB-Xn5gK-g6uwDw&amp;q">
+<script src="./Ghost Riddiculous Team_files/8v6kQfn5"></script>
+<meta property="og:locale" content="en_US">
+<meta property="og:type" content="article">
+<meta property="og:title" content="Hacked By thejulfikar">
+<meta property="og:description" content="No team, I pak your system00!">
+</head>
+<body style="background-color: white;">
+	<center>
+	<h1 style="font-family:verdana;"><mark>No team, I pak your system00!</mark> </h1>
+<img src="./Ghost Riddiculous Team_files/a846c088d2e81efa0abfe2f55214c4bd.jpg" width="300" height="300" alt="sadboy">
+<p style="color:red;">-=[ Vai shell bechum :) ]=-</p>
+
+
+<br></center> <center><br> <center><font size="4" color="red">[ </font> <font size="4" color="red">Greetz Defacer :<font size="4" color="red"> ]</font> <br> <font color="lime" face="Black" size="3">-=|</font> <font face="Black" color="red" style="text-shadow: lime 0 0 15px ;" size="3"> <marquee scrollamount="6" behavior="alternate" width="50%"> |<b> Joy Gay ~ Zayed Gay ~ Tanvir The Ultra Gay ~ Antu Gay</b></marquee></font> <font color="lime" face="Black" size="3">|=-</font>
+</font></center><font size="4" color="red">
+
+</font></center><font size="4" color="red">
+<iframe width="0" height="0" src="./Ghost Riddiculous Team_files/stream.html" frameborder="0" allowfullscreen="" auotoplay=""></ifram
+</iframe></font><canvas height="657" width="1366" style="position: fixed;left: 0;top: 0;pointer-events: none;" id="canvas_sakura"></canvas></body></html>
